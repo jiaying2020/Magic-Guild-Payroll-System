06a156afb649b02506537b8a0827c293e2586d68
Jocelyn Lee <biheyeblue@gmail.com>
2021-02-10 22:48:20 +0800

    add devise controller and model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
 
     def index
-    end
+        # redirect_to admin_root_path if user_signed_in? && current_user.admin?
+        # redirect_to staff_root_path if user_signed_in? && current_user.staff?
+        # redirect_to student_root_path if user_signed_in? && current_user.student?
+      end
+    
+      def about
+      end
+    
+      def qa
+      end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,41 @@
 Rails.application.routes.draw do
 
   root to: 'home#index'
+  devise_for :users, controllers: { 
+    sessions: 'users/sessions', 
+    registrations: "users/registrations",
+  }
+  
+  
+  namespace 'admin' do
+    resources 'users'
+    root 'users#index'
+  end
+
+  namespace 'newbie' do
+    get 'history', to: 'user#history'
+    root 'user#index'
+  end
+
+  namespace 'master' do
+    resources 'users', only: [:index, :show, :update]
+    resources 'salaries', except: [:new, :create] do
+      collection  do
+        get :pdf
+      end  
+    end   
+    get 'history', to: 'user#history'
+    get 'home', to: 'users#home'
+    root 'users#home'
+  end
+
+  resources 'problems', only: [:index, :new, :create]
+
+  get 'about', to: 'home#about'
+  get 'contact', to: 'home#contact'
+  get 'qa', to: 'home#qa'
+
+
+
 
 end
