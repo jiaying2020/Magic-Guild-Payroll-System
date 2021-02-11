f77a89420403593a447969243a66fd0be5be902f
Jocelyn Lee <biheyeblue@gmail.com>
2021-02-11 17:23:31 +0800

    add sign-in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,40 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root to: 'home#index'
-  devise_for :users, controllers: { 
-    sessions: 'users/sessions', 
-    registrations: "users/registrations",
-  }
+  # devise_for :users, controllers: { 
+  #   sessions: 'users/sessions', 
+  #   registrations: "users/registrations",
+  # }
+
   
-  
-  namespace 'admin' do
-    resources 'users'
-    root 'users#index'
-  end
+  # namespace 'admin' do
+  #   resources 'users'
+  #   root 'users#index'
+  # end
 
-  namespace 'newbie' do
-    get 'history', to: 'user#history'
-    root 'user#index'
-  end
+  # namespace 'newbie' do
+  #   get 'history', to: 'user#history'
+  #   root 'user#index'
+  # end
 
-  namespace 'master' do
-    resources 'users', only: [:index, :show, :update]
-    resources 'salaries', except: [:new, :create] do
-      collection  do
-        get :pdf
-      end  
-    end   
-    get 'history', to: 'user#history'
-    get 'home', to: 'users#home'
-    root 'users#home'
-  end
+  # namespace 'master' do
+  #   resources 'users', only: [:index, :show, :update]
+  #   resources 'salaries', except: [:new, :create] do
+  #     collection  do
+  #       get :pdf
+  #     end  
+  #   end   
+  #   get 'history', to: 'user#history'
+  #   get 'home', to: 'users#home'
+  #   root 'users#home'
+  # end
 
-  resources 'problems', only: [:index, :new, :create]
+  # resources 'problems', only: [:index, :new, :create]
 
-  get 'about', to: 'home#about'
-  get 'contact', to: 'home#contact'
-  get 'qa', to: 'home#qa'
+  # get 'about', to: 'home#about'
+  # get 'contact', to: 'home#contact'
+  # get 'qa', to: 'home#qa'
 
 
 
