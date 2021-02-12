579f34ee5d8036db750fc9afde97ebc13011cacc
Jocelyn Lee <biheyeblue@gmail.com>
2021-02-12 11:51:23 +0800

    edit novice page link

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
         # redirect_to admin_root_path if user_signed_in? && current_user.admin?
         # redirect_to staff_root_path if user_signed_in? && current_user.staff?
         # redirect_to student_root_path if user_signed_in? && current_user.student?
-        redirect_to novice_root_path if user_signed_in?
+        redirect_to novice_index_path if user_signed_in?
 
       end
     
